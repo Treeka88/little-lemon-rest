196844c1a63708410369ead1c7cea6b39e433842
Tarek.Y <tareq.yaghi@gmail.com>
2025-12-22 01:38:31 +0300

    some changes on reservation.js

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,23 +1,23 @@
+import { useState } from 'react';
+
 function Reservation(){
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [guests, setGuests] = useState(1);
+    const [occasion, setOccasion] = useState('');
+
     return(
         <div>
             <h1>Reservation Page</h1>
             <form style={{display: 'grid', gap: '20px', maxWidth: 200, margin: 'auto'}}>
                    <label for="res-date">Choose date</label>
-                    <input type="date" id="res-date"/>
+                    <input type="date" id="res-date" value={date} onChange={(e) => setDate(e.target.value)}/>
                     <label for="res-time">Choose time</label>
-                    <select id="res-time ">
-                        <option>17:00</option>
-                        <option>18:00</option>
-                        <option>19:00</option>
-                        <option>20:00</option>
-                        <option>21:00</option>
-                        <option>22:00</option>
-                    </select>
+                    <input type="time" id="res-time" value={time} onChange={(e) => setTime(e.target.value)}/>
                     <label for="guests">Number of guests</label>
-                    <input type="number" placeholder="1" min="1" max="10" id="guests"/>
+                    <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e) => setGuests(e.target.value)}/>
                     <label for="occasion">Occasion</label>
-                    <select id="occasion">
+                    <select id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
                         <option>Birthday</option>
                         <option>Anniversary</option>
                         <option>casual meeting</option>
