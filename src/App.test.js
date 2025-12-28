aa922dc6109502cd576aa79027734fe38e4e7e40
Tarek.Y <tareq.yaghi@gmail.com>
2025-12-28 03:16:57 +0300

    running some tests (didn't understand shit)

--- src/App.test.js
+++ src/App.test.js
@@ -1,37 +1,82 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 import { initializeTimes, updateTimes } from './App';
 import { fetchData } from './Api';
+import BookingForm from './components/BookingForm';
 
-jest.mock('./Api', () => ({
-  fetchData: jest.fn(),
-}));
 
-test('initializeTimes calls fetchData with today\'s date', () => {
-  const today = new Date();
-  const mockTimes = ['17:00', '18:00', '19:00'];
-  fetchData.mockReturnValue(mockTimes);
+// testing the initializeTimes and updateTimes functions
+// jest.mock('./Api', () => ({
+//   fetchData: jest.fn(),
+// }));
 
-  const times = initializeTimes();
+// test('initializeTimes calls fetchData with today\'s date', () => {
+//   const today = new Date();
+//   const mockTimes = ['17:00', '18:00', '19:00'];
+//   fetchData.mockReturnValue(mockTimes);
 
-  expect(fetchData).toHaveBeenCalledWith(today);
-  expect(times).toBe(mockTimes);
-}
-);
+//   const times = initializeTimes();
 
-test('updateTimes calls fetchData with the correct date', () => {
-  const mockTimes = ['17:00', '18:00', '19:00'];
-  fetchData.mockReturnValue(mockTimes);
+//   expect(fetchData).toHaveBeenCalledWith(today);
+//   expect(times).toBe(mockTimes);
+// }
+// );
 
-  const action = { type: 'UPDATE_TIMES', date: '2024-12-30' };
-  const expectedDate = new Date('2024-12-30');
+// test('updateTimes calls fetchData with the correct date', () => {
+//   const mockTimes = ['17:00', '18:00', '19:00'];
+//   fetchData.mockReturnValue(mockTimes);
 
-  const times = updateTimes([], action);
+//   const action = { type: 'UPDATE_TIMES', date: '2024-12-30' };
+//   const expectedDate = new Date('2024-12-30');
 
-  expect(fetchData).toHaveBeenCalledWith(expectedDate);
-  expect(times).toBe(mockTimes);
-}
-);
+//   const times = updateTimes([], action);
+
+//   expect(fetchData).toHaveBeenCalledWith(expectedDate);
+//   expect(times).toBe(mockTimes);
+// }
+// );
+
+
+// testing the BookingForm component
+// const mockProps = {
+//   date: '1-12-2025',
+//   setDate: jest.fn(),
+//   time: '10:00',
+//   setTime: jest.fn(),
+//   guests: '3',
+//   setGuests: jest.fn(),
+//   occasion: 'casual meeting',
+//   setOccasion: jest.fn(),
+//   availableTimes: ['17:00', '18:00', '19:00'],
+//   dispatch: jest.fn(),
+//   submitForm: jest.fn(),
+// };
+
+// test('testing the booking form with invalid number of guests', () => {
+//    render(<BookingForm{...mockProps}/>)
+//    const guestInput = screen.getByLabelText(/number of guests/i);
+//    fireEvent.change(guestInput, { target: {value: 'How many guests?'}})
+//    expect(
+//     screen.getByText(/number of guests must be between 1 and 10/i)).toBeInTheDocument();
+//    })
+
+  //  test('testing the booking form with valid number of guests', () => {
+  //   render(<BookingForm{...mockProps}/>)
+  //   const guestInput = screen.getByLabelText(/number of guests/i);
+  //   fireEvent.change(guestInput, { target: {value: '5'}})
+  //   expect(
+  //    screen.queryByText(/number of guests must be between 1 and 10/i)).not.toBeInTheDocument();
+  //   })
+
+   // testing the button is disabled when required fields are empty
+    // test('submit button is disabled when required fields are empty', () => {
+    //   render(<BookingForm{...mockProps}
+    //     guests={'how many guests?'}/>)
+    //   const submitButton = screen.getByRole('button', { name: /make your reservation/i });
+    //   expect(submitButton).toBeDisabled();
+    // })
+
+
 
 // test('renders learn react link', () => {
 //   render(<App />);
